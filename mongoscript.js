6f88258be183491e9c03477eb1a5b81a30193320
ranjith reynold <ranjithreynold75@gmail.com>
2016-12-28 11:03:45 -0800

    everything

---
+++ mongoscript.js
@@ -0,0 +1,2 @@
+db.user.insert({_id:'8754623583',username:"ranjith",email:"ranjithreynold@gmail.com",password:"qwerty",location:{logitude:"123",latitude:"123"},status:"online",access:"not verified"})
+db.user.find()
