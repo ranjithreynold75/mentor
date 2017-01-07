3d38fb6f7703f6abdf7f8f9dee4cbdf17951bccb
ranjith reynold <ranjithreynold75@gmail.com>
2017-01-06 22:23:41 -0800

    profile pic request update

--- public/app.js
+++ public/app.js
@@ -1,15 +1,18 @@
 var express=require('express');
 //var connect=require('connect');
 var app=express();
+var bodyparser=require('body-parser');
 
 function notify(req,res,next)
 {
-    console.log("request been made");
+    console.log("request been made"+req.url);
     next();
 }
 app.use(notify);
 app.use(express.static(__dirname+'/public'))
-
+app.use(express.static(__dirname+'/favicon.ico'))
+app.use(bodyparser.json({limit: '50mb'}));
+app.use(bodyparser.urlencoded({limit: '50mb', extended: true}));
 require('./routes.js')(app);
 
 var server=app.listen(8000,function(){
--- public/routes.js
+++ public/routes.js
@@ -10,7 +10,34 @@ var urlencoder=bodyparser.urlencoded({extended:false});
 module.exports=function(app) {
 
 
-    app.post('/user_signup',urlencoder, function (req, res)
+    //user request for profile pic
+    app.post('/pro_pic',urlencoder,function(req,res){
+        var phone=req.body.phone;
+        console.log("profilepic request by "+phone);
+        //var data=fs.readFileSync("./profile/"+phone+".jpg");
+
+        fs.readFile("D:/mentor/profile/"+phone+".jpg",function(err,data){
+            if(err)
+                console.log("error retrivel");
+            else
+            {
+                var profilepic=new Buffer(data).toString("base64");
+                console.log('encoded success');
+                res.send(profilepic);
+
+            }
+        })
+    })
+
+
+
+
+
+
+
+
+
+    app.post('/user_signup',urlencoder,function (req, res)
     {
 mc.connect(url,function(err,db){
     if(err)
@@ -27,6 +54,20 @@ var curser=collection.find({_id:req.body.phone});
         curser.count(function(err,c){
 
     if(c==0) {
+
+var image=req.body.profile_image;
+        var bitmap=new Buffer(image,"base64");
+        fs.writeFile("./profile/"+req.body.phone+".jpg",bitmap,function(err,data){
+            if(err)
+            {
+                console.log("error on picture upload");
+            }
+            else
+            {
+                console.log("picture uploaded");
+            }
+
+        })
         data = {
             _id: req.body.phone,
             username: req.body.username,
@@ -41,8 +82,8 @@ var curser=collection.find({_id:req.body.phone});
             state: req.body.state,
             status: "online",
             access: "not verified",
-            shop_owner: "no"
-
+            shop_owner: "no",
+            profile_image:"/profile/"+req.body.phone+".jpg"
         };
 
 
@@ -88,60 +129,33 @@ var curser=collection.find({_id:req.body.phone});
              {
                  var collection=db.collection('user');
 
-                 var cursor=collection.find({_id:phone,password:password})
+                 collection.find({_id:phone,password:password}).forEach(function(x){
 
-cursor.count(function(err,c){
-                     if(c==1)
+                     if(phone==x._id && password==x.password)
                      {
+                         console.log(phone+" logged in");
+                         res.send('success');
 
-                      console.log("success");
-                         res.send("success");
+                     }
+                     else{
+                         res.send('invalid');
                      }
 
-                     else
-                     {
-                     
-                                              console.log("invalid");
-                         res.send("invalid");
-                     }
+                 })
 
-})
+
              }
 
          })
 
 
-    })
-/*
-app.get('/',function(req,res){
-
-    mc.connect(url,function(err,db){
-        if(err)
-            console.log("db error");
-
-        else
-        {
-            var collection=db.collection('user');
-            /*var cursor=collection.find({_id:'8754623583'});
-cursor.each(function(err,d){
-    console.log(d);
-})
-            collection.find({_id:'8754623583'}).forEach(function(x){
-                console.log(x.username);
-                if(x.username=='ranjith reynold')
-                    res.send("hai moto");
-            })
-
-        }
-
-    })
+    });
 
 
 
-}) */
 app.get('/',function(req,res){
     res.send("welcome to mentor");
-})
+});
 
 
 }
